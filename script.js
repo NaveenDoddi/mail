2cb58cdeba87e1257f6e3c239a5e5263d1950ff3
naveendoddi <prasadnaveen847@gmail.com>
2023-04-14 23:03:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+function run(){
+    var str = document.getElementById("input").value.toLowerCase().split("")
+    var alpha = "abcdefghijklmnopqrstuvwxyz".split("")
+    var num = "1234567890".split("")
+    
+    var alphabool = false, numbool = false, Abool = false, dotbool = false, combool = false, legthbool = true, spacebool = true
+    
+    const tlds = [".com", ".org", ".net", ".edu", ".gov", ".mil", ".int", ".info", ".biz", ".pro", ".name", ".coop", ".mobi", ".asia", ".eu", ".aero", ".cat", ".jobs", ".tel", ".travel"];
+    
+    var Acounting = 0
+    for(let i = 0; i < str.length; i++){
+        if(str[i]==" "){
+            spacebool = false
+        }
+        if(str[i]=="@"){
+            Abool = true
+            if(str[i+1]=="."){
+                Abool = false
+            }
+            Acounting++
+    
+            if(Acounting>1){
+                Abool = false
+            }
+            
+        }
+        if(str[i]=="."){
+            dotbool = true
+            let com = str.slice(i).join("")
+            combool = tlds.some((i)=> i==com)
+    
+            if(str[i+1]=="."){
+                dotbool = false
+            }
+            
+        }
+        for(let j = 0; j < alpha.length; j++){
+            if(str[i]==str[j]){
+                alphabool = true
+            }
+            
+        }
+        for(let j = 0; j < num.length; j++){
+            if(str[i]==str[j]){
+               numbool = true
+            }
+            
+        }
+    
+    }
+    if(str[str.length-1] == "."){
+        dotbool = false
+    }
+    if(str[str.length-1] == "@" || str[0]=="@"){
+        Abool = false
+    }
+    if(str.lenght>64){
+        legthbool = false
+    }
+    if(alphabool && numbool && Abool && dotbool && combool && legthbool && spacebool){
+        alert("valid")
+    }else{
+        alert("invalid")
+    }
+}
+
+// console.log(str)
